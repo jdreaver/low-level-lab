216542d38b83b5a850d49ee872c41e3179afecc7
David Reaver <johndreaver@gmail.com>
2021-09-19 09:05:37 -0700

    More work on copy.c

--- linux-programming-interface/ch4-file-io/copy.c
+++ linux-programming-interface/ch4-file-io/copy.c
@@ -1,3 +1,4 @@
+#include <fcntl.h>  /* open */
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>     /* Prototypes of commonly used library functions,
@@ -7,6 +8,16 @@
 #ifndef BUF_SIZE
 #define BUF_SIZE 1024
 #endif
+
+#ifdef TRUE
+#undef TRUE
+#endif
+
+#ifdef FALSE
+#undef FALSE
+#endif
+
+typedef enum { FALSE, TRUE } Boolean;
 
 void usageErr(const char *format, ...) {
   va_list argList;
@@ -22,17 +33,29 @@ void usageErr(const char *format, ...) {
   exit(EXIT_FAILURE);
 }
 
+void errExit(const char *format, ...) {
+  va_list argList;
+
+  va_start(argList, format);
+  outputError(TRUE, errno, TRUE, format, argList);
+  va_end(argList);
+
+  terminate(TRUE);
+}
+
 int main(int argc, char *argv[]) {
-  printf("Hello!\n");
+  int inputFile;
 
-  printf("BUF_SIZE: %d\n", BUF_SIZE);
-
-  int i;
-  for (i = 0; i < argc; i++) {
-    printf("arg %d: %s\n", i, argv[i]);
-  }
+  /* for (int i = 0; i < argc; i++) { */
+  /*   printf("arg %d: %s\n", i, argv[i]); */
+  /* } */
 
   if (argc != 3 || strcmp(argv[1], "--help") == 0) {
     usageErr("%s old-file new-file\n", argv[0]);
+  }
+
+  inputFile = open(argv[1], O_RDONLY);
+  if (inputFile == -1) {
+
   }
 }
