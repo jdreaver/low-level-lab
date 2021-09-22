bf929d4e37ffdfea4513fb01f5458dc6bb46b58e
David Reaver <johndreaver@gmail.com>
2021-09-21 18:32:49 -0700

    Finish copy.c

--- linux-programming-interface/ch4-file-io/copy.c
+++ linux-programming-interface/ch4-file-io/copy.c
@@ -1,9 +1,11 @@
+#include <errno.h>
 #include <fcntl.h>  /* open */
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>     /* Prototypes of commonly used library functions,
                            plus EXIT_SUCCESS and EXIT_FAILURE constants */
 #include <string.h>     /* Commonly used string-handling functions */
+#include <unistd.h>	/* read */
 
 #ifndef BUF_SIZE
 #define BUF_SIZE 1024
@@ -33,29 +35,63 @@ void usageErr(const char *format, ...) {
   exit(EXIT_FAILURE);
 }
 
-void errExit(const char *format, ...) {
-  va_list argList;
+void errExit(const char *userText) {
+  char *errorStr;
+  errorStr = strerror(errno);
 
-  va_start(argList, format);
-  outputError(TRUE, errno, TRUE, format, argList);
-  va_end(argList);
+  fprintf(stderr, "ERROR %s (%s)\n", userText, errorStr);
+  fflush(stderr);           /* In case stderr is not line-buffered */
+  exit(EXIT_FAILURE);
+}
 
-  terminate(TRUE);
+void fatal(const char *userText) {
+  fprintf(stderr, "%s\n", userText);
+  fflush(stderr);           /* In case stderr is not line-buffered */
+  exit(EXIT_FAILURE);
 }
 
 int main(int argc, char *argv[]) {
-  int inputFile;
-
-  /* for (int i = 0; i < argc; i++) { */
-  /*   printf("arg %d: %s\n", i, argv[i]); */
-  /* } */
-
   if (argc != 3 || strcmp(argv[1], "--help") == 0) {
     usageErr("%s old-file new-file\n", argv[0]);
   }
 
-  inputFile = open(argv[1], O_RDONLY);
+  char buf[BUF_SIZE];
+  int inputFile = open(argv[1], O_RDONLY);
   if (inputFile == -1) {
-
+    snprintf(buf, BUF_SIZE, "Failed to open %s", argv[1]);
+    errExit(buf);
   }
+
+  int openFlags = O_CREAT | O_WRONLY | O_TRUNC;
+  mode_t filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH; /* rw-rw-rw- */
+  int outputFile = open(argv[2], openFlags, filePerms);
+  if (outputFile == -1) {
+    snprintf(buf, BUF_SIZE, "Failed to open %s", argv[1]);
+    errExit(buf);
+  }
+
+  /* Transfer data until we encounter end of input or an error */
+  int numRead;
+
+  while ((numRead = read(inputFile, buf, BUF_SIZE)) > 0) {
+    if (numRead == -1) {
+      errExit("error reading from input file");
+    }
+    int numWrite = write(outputFile, buf, numRead);
+    if (numWrite == -1) {
+      errExit("error writing to file");
+    } else if (numWrite != numRead) {
+      fatal("failed to write whole buffer");
+    }
+  }
+
+  if (close(inputFile) == -1) {
+    errExit("closing input");
+  }
+
+  if (close(outputFile) == -1) {
+    errExit("closing output");
+  }
+
+  exit(EXIT_SUCCESS);
 }
