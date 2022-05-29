40c250aa54a3f9b85cafbf84e86e49448f94acfd
David Reaver <me@davidreaver.com>
2022-05-29 12:17:21 -0700

    Create vm-compiler.rs

---
+++ rust/src/bin/vm-compiler.rs
@@ -0,0 +1,21 @@
+use std::env;
+use std::fs;
+use std::process;
+
+use nand2tetris::vm;
+
+fn main() {
+    let source_path: String = match env::args().collect::<Vec<String>>().as_slice() {
+        [_, path] => path.to_string(),
+        _ => {
+            eprintln!("Usage: assembler <filename>");
+            process::exit(1);
+        }
+    };
+
+    let source_string =
+        fs::read_to_string(source_path).expect("Something went wrong reading the source file");
+
+    let parsed = vm::parse_vm_source(&source_string).expect("failed to parse source");
+    println!("{:#?}", parsed);
+}
