d3d18ddf64a473bf7f283918aacc0fcf5e982a34
David Reaver <me@davidreaver.com>
2022-05-30 13:56:16 -0700

    Have vm-compiler write to multiple files in place

--- rust/src/bin/vm-compiler.rs
+++ rust/src/bin/vm-compiler.rs
@@ -6,29 +6,30 @@ use std::process;
 use nand2tetris::vm;
 
 fn main() {
-    let source_path: String = match env::args().collect::<Vec<String>>().as_slice() {
-        [_, path] => path.to_string(),
-        _ => {
-            eprintln!("Usage: assembler <filename>");
-            process::exit(1);
-        }
-    };
+    if env::args().len() <= 1 {
+        eprintln!("Usage: assembler <filename>");
+        process::exit(1);
+    }
 
-    let file_basename: &str = Path::new(&source_path)
-        .file_stem()
-        .expect("couldn't get file basename")
-        .to_str()
-        .expect("couldn't convert file basename to &str");
+    for path_str in env::args().skip(1) {
+        let path = Path::new(&path_str);
+        let file_basename: &str = path
+            .file_stem()
+            .expect("couldn't get file basename")
+            .to_str()
+            .expect("couldn't convert file basename to &str");
 
-    let source_string = fs::read_to_string(source_path.clone())
-        .expect("Something went wrong reading the source file");
+        let source_string =
+            fs::read_to_string(path.clone()).expect("Something went wrong reading the source file");
 
-    let parsed = vm::parse_vm_source(&source_string).expect("failed to parse source");
-    // println!("{:#?}", parsed);
+        let parsed = vm::parse_vm_source(&source_string).expect("failed to parse source");
+        // println!("{:#?}", parsed);
 
-    let asm =
-        vm::vm_to_asm(&file_basename.to_string(), parsed).expect("failed to compile source to ASM");
-    for line in &asm {
-        println!("{}", line)
+        let asm = vm::vm_to_asm(&file_basename.to_string(), parsed)
+            .expect("failed to compile source to ASM")
+            .join("\n");
+
+        let output_file_path = path.with_extension("asm");
+        fs::write(output_file_path, format!("{}\n", asm)).expect("failed to write to output file");
     }
 }
