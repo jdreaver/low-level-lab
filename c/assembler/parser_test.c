d0cab10d0bdf310f84cfae2b3d500cb2a30b725b
David Reaver <me@davidreaver.com>
2022-06-05 09:51:23 -0700

    Use Unity test framework

--- c/assembler/parser_test.c
+++ c/assembler/parser_test.c
@@ -1,3 +1,5 @@
+#include "unity.h"
+
 #include <assert.h>
 #include <string.h>
 
@@ -5,66 +7,59 @@
 // N.B. Including whole source file to get at static functions
 #include "parser.c"
 
-void assert_strings_equal(char *s1, char *s2, const char *__file, unsigned int __line, const char *__function) {
-	if (strcmp(s1, s2) != 0) {
-		fprintf(stderr, "%s:%u: %s: Strings not equal!\ns1: %s\ns2: %s\n", __file, __line, __function, s1, s2);
-		exit(EXIT_FAILURE);
-	}
-}
+void setUp (void) {} /* Is run before every test, put unit init calls here. */
+void tearDown (void) {} /* Is run after every test, put unit clean-up calls here. */
 
 void test_eat_whitespace_comments()
 {
-	puts("Testing empty line");
 	char *empty_line = "";
 	eat_whitespace_comments(&empty_line);
-	assert_strings_equal(empty_line, "", __FILE__, __LINE__, __func__);
+	TEST_ASSERT_EQUAL_STRING("", empty_line);
 
-	puts("Testing comment line");
 	char *simple_comment = "   // asdskljlk dfslkjk dslk // sdf// //";
 	eat_whitespace_comments(&simple_comment);
-	assert_strings_equal(simple_comment, "", __FILE__, __LINE__, __func__);
+	TEST_ASSERT_EQUAL_STRING("", simple_comment);
 
-	puts("Testing comment line followed by non-comment");
 	char *non_comment = "   // asdskljlk dfslkjk dslk // sdf// //\nhello!";
 	eat_whitespace_comments(&non_comment);
-	assert_strings_equal(non_comment, "hello!", __FILE__, __LINE__, __func__);
+	TEST_ASSERT_EQUAL_STRING("hello!", non_comment);
 
-	puts("Testing comment line followed by non-comment and space");
 	char *non_comment_then_space = "   // asdskljlk dfslkjk dslk // sdf// //\n   hello!";
 	eat_whitespace_comments(&non_comment_then_space);
-	assert_strings_equal(non_comment_then_space, "hello!", __FILE__, __LINE__, __func__);
+	TEST_ASSERT_EQUAL_STRING("hello!", non_comment_then_space);
 }
 
 
 void test_parse_a_instruction()
 {
-	puts("Testing no @");
+
+	// Missing @ symbol
 	char *input = "";
 	struct asm_a_instruction instruction;
 	enum asm_parse_error err = parse_a_instruction(&input, &instruction);
-	assert(err == ASM_PARSE_ERROR_A_INSTRUCTION_MISSING_AT_SYMBOL);
+	TEST_ASSERT_EQUAL_INT(ASM_PARSE_ERROR_A_INSTRUCTION_MISSING_AT_SYMBOL, err);
 
-	puts("Testing simple @123");
+	// Simple success
 	input = "@123";
 	err = parse_a_instruction(&input, &instruction);
-	assert(err == ASM_PARSE_ERROR_NO_ERROR);
-	assert(instruction.type == ASM_A_INST_ADDRESS);
-	assert(instruction.address == 123);
+	TEST_ASSERT_EQUAL_INT(ASM_PARSE_ERROR_NO_ERROR, err);
+	TEST_ASSERT_EQUAL_INT(ASM_A_INST_ADDRESS, instruction.type);
+	TEST_ASSERT_EQUAL_UINT16(123, instruction.address);
 
-	puts("Testing bare @");
+	// Bare @
 	input = "@";
 	err = parse_a_instruction(&input, &instruction);
-	assert(err == ASM_PARSE_ERROR_UNEXPECTED_EOF);
+	TEST_ASSERT_EQUAL_INT(ASM_PARSE_ERROR_UNEXPECTED_EOF, err);
 
-	puts("Testing logical overflow @32768");
+	// Logical overflow (fits in uint16_t, but is too large for address)
 	input = "@32768";
 	err = parse_a_instruction(&input, &instruction);
-	assert(err == ASM_PARSE_ERROR_A_INSTRUCTION_ADDRESS_TOO_LARGE);
+	TEST_ASSERT_EQUAL_INT(ASM_PARSE_ERROR_A_INSTRUCTION_ADDRESS_TOO_LARGE, err);
 
-	puts("Testing uint16_t overflow @32768231432848329789437289");
+	// uint16_t overflow
 	input = "@32768231432848329789437289";
 	err = parse_a_instruction(&input, &instruction);
-	assert(err == ASM_PARSE_ERROR_A_INSTRUCTION_ADDRESS_TOO_LARGE);
+	TEST_ASSERT_EQUAL_INT(ASM_PARSE_ERROR_A_INSTRUCTION_ADDRESS_TOO_LARGE, err);
 
 	// TODO: Add this once we support labels, since this goes down label path
 	/* puts("Testing negative @-398"); */
@@ -73,10 +68,10 @@ void test_parse_a_instruction()
 	/* assert(err == ASM_PARSE_ERROR_A_INSTRUCTION_ADDRESS_TOO_LARGE); */
 }
 
-int main()
+int main(void)
 {
-	puts("test_eat_whitespace_comments()");
-	test_eat_whitespace_comments();
-	puts("test_parse_a_instruction()");
-	test_parse_a_instruction();
+	UNITY_BEGIN();
+	RUN_TEST(test_eat_whitespace_comments);
+	RUN_TEST(test_parse_a_instruction);
+	return UNITY_END();
 }
---
+++ c/unity/update.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Updates the unity test framework files
+
+set -eux
+
+wget https://raw.githubusercontent.com/ThrowTheSwitch/Unity/master/src/unity.c
+wget https://raw.githubusercontent.com/ThrowTheSwitch/Unity/master/src/unity.h
+wget https://raw.githubusercontent.com/ThrowTheSwitch/Unity/master/src/unity_internals.h
