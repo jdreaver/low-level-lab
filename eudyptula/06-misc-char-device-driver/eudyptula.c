edc7159f338ac6fd610644e9ef33a47a36f50ac2
David Reaver <me@davidreaver.com>
2022-06-27 19:58:50 -0700

    Remove warnings

--- eudyptula/06-misc-char-device-driver/eudyptula.c
+++ eudyptula/06-misc-char-device-driver/eudyptula.c
@@ -10,30 +10,32 @@ MODULE_DESCRIPTION("Simple misc char device driver for eudyptula task 6");
 MODULE_VERSION("0.1");
 
 static char *message = "7c1caf2f50d1";
+#define message_len 13 // Length of message + null byte
 
 static ssize_t misc_read(struct file *filp, char __user *buf, size_t count, loff_t *ppos)
 {
 	pr_info("in %s misc_read", THIS_MODULE->name);
-	return simple_read_from_buffer(buf, count, ppos, message, strlen(message));
+	return simple_read_from_buffer(buf, count, ppos, message, message_len);
 }
 
 static ssize_t misc_write(struct file *filp, const char __user *buf, size_t count, loff_t *ppos)
 {
+	char copied[message_len];
+
 	pr_info("in %s misc_write. count: %zu\n", THIS_MODULE->name, count);
 
 	// We can't just strncmp(message, buf, count) because the passed in
 	// buffer is a userspace pointer. We first have to copy it to kernel
 	// space.
-	char copied[count + 1];
-	if ((simple_write_to_buffer(copied, count, ppos, buf, count) < 0)) {
+	if ((simple_write_to_buffer(copied, count, ppos, buf, message_len) < 0)) {
 		pr_crit("simple_write_to_buffer failed!\n");
 		return -EINVAL;
 	}
-	copied[count] = '\0';
+	copied[message_len - 1] = '\0';
 
-	pr_info("copied message: %s, strncmp() = %d\n", copied, strncmp(message, copied, strlen(message)));
-	if (strncmp(message, copied, strlen(message)) == 0) {
-		return strlen(message); // TODO: Incorrect?
+	pr_info("copied message: %s, strncmp() = %d\n", copied, strncmp(message, copied, message_len));
+	if (strncmp(message, copied, message_len) == 0) {
+		return message_len - 1;
 	}
 	return -EINVAL;
 }
