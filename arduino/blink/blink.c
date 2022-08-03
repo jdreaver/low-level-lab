c7c2bf735fc49fabd3ade56051a4778bdb2e56ef
David Reaver <me@davidreaver.com>
2022-08-03 15:00:38 -0700

    Get Arduino blink working without avr-gcc

--- arduino/blink/blink.c
+++ arduino/blink/blink.c
@@ -1,7 +1,4 @@
-#include <avr/io.h>
-#include <util/delay.h>
-
-#define MS_DELAY 2000
+#include <stdint.h>
 
 // On the ATMEGA2560, the builtin LED is on pin 13 just like the Arduino Uno.
 // However, digital pin 13 goes to PB7 (see
@@ -9,18 +6,28 @@
 #define LED_PORTB PORTB7
 #define LED_DDB DDB7
 
+#define DDRB (*(volatile uint8_t *)0x24)
+#define DDB7 (1 << 7)
+#define LED_DDB DDB7
+
+#define PORTB (*(volatile uint8_t *)0x25)
+#define PORTB7 (1 << 7)
+#define LED_PORTB PORTB7
+
 int main()
 {
 	// Initialize LED to output mode
-	DDRB |= _BV(LED_DDB);
+	DDRB |= LED_DDB;
 
 	while(1) {
-		// Set pin to HIGH (turn on LED)
-		PORTB |= _BV(LED_PORTB);
-		_delay_ms(MS_DELAY);
+		// Set pin to HIGH (turn on LED). Also loop here to simulate delay.
+		for (uint32_t i = 0; i < 1000000; i++) {
+			PORTB |= LED_PORTB;
+		}
 
 		// Set pin to LOW (turn off LED)
-		PORTB &= ~_BV(LED_PORTB);
-		_delay_ms(MS_DELAY);
+		for (uint32_t i = 0; i < 1000000; i++) {
+			PORTB &= ~LED_PORTB;
+		}
 	}
 }
