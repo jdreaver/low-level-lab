15480e59cf5a660355bbf6a2f9b7738c9ecf63d6
David Reaver <me@davidreaver.com>
2022-08-07 19:46:34 -0700

    Implement common files as actual C files

--- stm32/common/usart2.h
+++ stm32/common/usart2.h
@@ -3,7 +3,7 @@
  *
  * To use this module, you must call `usart2_enable()`. You probably also want
  * to override _write so newlib knows where to send e.g. puts() and printf()
- * output (we can't/shouldn't do this in a header-only library).
+ * output. (TODO: Can we do this with some kind of option?)
  *
  *     // Override the 'write' newlib method to implement 'printf' over USART.
  *     int _write(int handle, char* data, int size) {
@@ -13,56 +13,5 @@
 
 #pragma once
 
-#ifdef STM32F401xE
-
-#include "stm32f4xx.h"
-
-// Default system clock is HSI clock source, which is 16MHz
-#define BAUD_RATE 9600
-#define DEFAULT_HSI_CLOCK_HZ 16000000UL
-
-void usart2_enable(void)
-{
-	// Pins PA3 and PA2 are the USART pins on STM32F401RE. We need to enable
-	// GPIOA and set the MODER for PA2/PA3 to Alternate Function Mode (10).
-	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
-	GPIOA->MODER |= (0b10 << GPIO_MODER_MODE2_Pos);
-	GPIOA->MODER |= (0b10 << GPIO_MODER_MODE3_Pos);
-
-	// Need to configure what alternate function the pins will perform. See
-	// Figure 17. Selecting an alternate function on STM32F401xB/C and
-	// STM32F401xD/E on pg 151 of reference manual. Need to set these to AF7
-	// for USART2.
-	GPIOA->AFR[0] |= (0b0111 << GPIO_AFRL_AFSEL2_Pos);
-	GPIOA->AFR[0] |= (0b0111 << GPIO_AFRL_AFSEL3_Pos);
-
-	// USART2 is located on APB1
-	RCC->APB1ENR |= (RCC_APB1ENR_USART2EN);
-
-	// Enable USART2
-	USART2->CR1 |= USART_CR1_UE; // USART enable
-	USART2->CR1 |= USART_CR1_TE; // Transmitter enable
-	USART2->CR1 |= USART_CR1_RE; // Receiver enable
-
-	// Compute baud rate register
-	uint16_t uart_div = DEFAULT_HSI_CLOCK_HZ / BAUD_RATE;
-	uint16_t brr = (uart_div / 16) << USART_BRR_DIV_Mantissa_Pos;
-	brr |= (uart_div % 16) << USART_BRR_DIV_Fraction_Pos;
-	USART2->BRR = brr;
-
-	// Enable the USART peripheral.
-	USART2->CR1 |= (USART_CR1_RE | USART_CR1_TE | USART_CR1_UE);
-}
-
-int usart2_write(__attribute__((unused)) int handle, char* data, int size) {
-	int count = size;
-	while(count--) {
-		while(!(USART2->SR & USART_SR_TXE)) {};
-		USART2->DR = *data++;
-	}
-	return size;
-}
-
-#else
-  #error "Unknown board. Can't set up usart2.h"
-#endif
+void usart2_enable(void);
+int usart2_write(int handle, char* data, int size);
---
+++ stm32/common/user_led.c
@@ -0,0 +1,59 @@
+#include "user_led.h"
+
+#ifdef STM32F401xE
+
+#include "stm32f4xx.h"
+
+void user_led_enable(void)
+{
+	// Need to enable the AHB1 peripheral clock. See Section 6.3.9 RCC AHB1
+        // peripheral clock enable register (RCC_AHB1ENR) in the Reference
+        // Manual. AHB1 is offset 0x30 from RCC, and GPIOA is bit 0 in register.
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+
+	// Enable PA5 (pin 5 of GPIOA) as an output bit, which controls the LED.
+	// (All of GPIOA_MODER is set to 0 on reset, so we just need to set our
+	// one bit to set MODER5 to 01.)
+	//
+        // From User Manual section 6.4 LEDs: "User LD2: the green LED is a user
+        // LED connected to ARDUINO® signal D13 corresponding to STM32 I/O PA5
+        // (pin 21) or PB13 (pin 34) depending on the STM32 target. Refer to
+        // Table 11 to Table 23 when: - the I/O is HIGH value, the LED is on -
+        // the I/O is LOW, the LED is off"
+        //
+        // Figure 18. NUCLEO-F401RE and Table 16. ARDUINO® connectors on NUCLEO-F401RE
+        // and NUCLEO-F411RE have PA5 next to D13, and PA5 == Pin 5 of GPIOA
+	//
+	// (Information needed if we weren't using stm32f401xe.h header files)
+	// MODER is offset of 0x0 from register base. MODER5 is bits 10/11 (see
+	// 8.4.1 GPIO port mode register (GPIOx_MODER) (x = A..E and H) on page
+	// 158 of Reference Manual). Need to set bits 10/11 to 01 for output
+	// mode.
+	GPIOA->MODER |= GPIO_MODER_MODER5_0;
+	GPIOA->MODER &= ~(GPIO_MODER_MODER5_1);
+
+	// (Information needed if we weren't using stm32f401xe.h header files)
+	// ODR is offset 0x14 from GPIO base, and we want pin 5, which is the
+	// 5th bit. See 8.4.6 GPIO port output data register (GPIOx_ODR) (x =
+	// A..E and H) on page 160 of reference manual.
+}
+
+void user_led_turn_on(void)
+{
+	GPIOA->ODR |= GPIO_ODR_OD5;
+}
+
+void user_led_turn_off(void)
+{
+	GPIOA->ODR &= ~(GPIO_ODR_OD5);
+}
+
+
+void user_led_toggle(void)
+{
+	GPIOA->ODR ^= GPIO_ODR_OD5;
+}
+
+#else
+  #error "Unknown board. Can't set up user_led.c"
+#endif
