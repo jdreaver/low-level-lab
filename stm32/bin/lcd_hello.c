3c7f37ba5177dc7f444f23d68fd9affa0de9b5b3
David Reaver <me@davidreaver.com>
2022-08-13 13:13:22 -0700

    Use 3 GPIOA pins (the C pins were being funky)

--- stm32/bin/lcd_hello.c
+++ stm32/bin/lcd_hello.c
@@ -1,44 +1,42 @@
 /**
  * Hello world for HD44780U LCD screen.
  *
- * Used just GPIOC pins. I need 11 pins for control, and there are only 10 GPIOC
- * pins on the left of my Nucleo STM32F401RE, so one pin (PC7) is on the right
- * side of the board.
  */
 
 #include "hd44780u_lcd.h"
 #include "system_clock.h"
 #include "usart2.h"
 
-#define make_lcd_pin(num) { \
-	.mode_reg = &GPIOC->MODER, \
+#define make_lcd_pin(gpio, num) { \
+	.mode_reg = &GPIO ## gpio->MODER, \
 	.mode_reg_clear_mask = ~(GPIO_MODER_MODE ## num ## _0 | GPIO_MODER_MODE ## num ## _1), \
 	.mode_reg_output_mask = GPIO_MODER_MODE ## num ## _0,		\
 	.mode_reg_input_mask = 0,					\
-	.output_data_reg = &GPIOC->ODR,					\
+	.output_data_reg = &GPIO ## gpio->ODR,			\
 	.output_data_reg_mask = GPIO_ODR_OD ## num,			\
 	}
 
 struct hd44780u_lcd lcd = {
 	.delay_microseconds_fn = systick_delay_microseconds,
-	.rs_pin = make_lcd_pin(11),
-	.rw_pin = make_lcd_pin(10),
-	.e_pin = make_lcd_pin(12),
+	.rs_pin = make_lcd_pin(C, 11),
+	.rw_pin = make_lcd_pin(C, 10),
+	.e_pin = make_lcd_pin(C, 12),
 	.data_pins = {
-		make_lcd_pin(13),
-		make_lcd_pin(14),
-		make_lcd_pin(15),
-		make_lcd_pin(2),
-		make_lcd_pin(1),
-		make_lcd_pin(3),
-		make_lcd_pin(0),
-		make_lcd_pin(7),
+		make_lcd_pin(A, 13),
+		make_lcd_pin(A, 14),
+		make_lcd_pin(A, 15),
+		make_lcd_pin(C, 2),
+		make_lcd_pin(C, 1),
+		make_lcd_pin(C, 3),
+		make_lcd_pin(C, 0),
+		make_lcd_pin(C, 7),
 	},
 };
 
 void start(void)
 {
-	// Enable GPIOC since all of our pins are GPIOC
+	// Enable GPIO ports
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
 
 	usart2_enable();
