51f0bce21b2e709e881379d3bb052ae11e37b75c
David Reaver <me@davidreaver.com>
2022-08-13 15:44:25 -0700

    Select message when button presses

--- stm32/bin/lcd_hello.c
+++ stm32/bin/lcd_hello.c
@@ -1,11 +1,12 @@
 /**
  * Hello world for HD44780U LCD screen.
- *
  */
 
 #include "hd44780u_lcd.h"
 #include "system_clock.h"
-#include "usart2.h"
+#include "user_button.h"
+
+#include <stddef.h>
 
 #define make_lcd_pin(gpio, num) { \
 	.mode_reg = &GPIO ## gpio->MODER, \
@@ -29,16 +30,41 @@ struct hd44780u_lcd lcd = {
 	},
 };
 
+char *messages[] = {
+	"Hello, Amy!",
+	"Bum diggity",
+};
+
+#define NUM_MESSAGES (sizeof messages / sizeof messages[0]);
+
+volatile size_t message_index = 0;
+
+void refresh_message(void)
+{
+	hd44780u_lcd_clear(&lcd);
+	hd44780u_lcd_write_string(&lcd, messages[message_index]);
+}
+
 void start(void)
 {
 	// Enable GPIO ports
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
 
-	usart2_enable();
+	user_button_enable();
 	systick_enable_passive();
 
 	hd44780u_lcd_init(&lcd);
-	hd44780u_lcd_write_string(&lcd, "Hello,");
-	hd44780u_lcd_write_string(&lcd, " Bob!");
+	refresh_message();
+}
+
+void EXTI15_10_IRQHandler(void) {
+	if (EXTI->PR & (1 << USER_BUTTON_PIN)) {
+		// Clear the EXTI status flag.
+		EXTI->PR |= (1 << USER_BUTTON_PIN);
+
+		// Pick next message
+		message_index = (message_index + 1) % NUM_MESSAGES;
+		refresh_message();
+	}
 }
