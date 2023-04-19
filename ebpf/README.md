581a80dabfe122eea52a291a9017fad3f7f73d5f
David Reaver <me@davidreaver.com>
2023-04-19 10:53:57 -0700

    Count user syscalls

---
+++ ebpf/README.md
@@ -0,0 +1,5 @@
+# eBPF
+
+Contains stuff from:
+- [Learning eBPF](https://www.amazon.com/Learning-eBPF-Programming-Observability-Networking/dp/1098135121)
+  - <https://github.com/lizrice/learning-ebpf>
---
+++ ebpf/count-user-syscall/count-user-syscall.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+# N.B. To run on NixOS with a shell, we need to run with sudo via:
+# $ sudo --preserve-env env PATH="$PATH" PYTHONPATH="$PYTHONPATH" ./count-user-execve.py
+
+from bcc import BPF
+import time
+
+# This program counts the number of syscalls per user ID. It uses the
+# BPF_HASH() table to store the counts, and the Python wrapper prints them.
+program = r"""
+BPF_HASH(counter_table);
+
+int hello(void *ctx) {
+  u64 uid;
+  u64 counter = 0;
+  u64 *p;
+
+  uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
+  p = counter_table.lookup(&uid);
+  if (p != 0) {
+    counter = *p;
+  }
+  counter++;
+  counter_table.update(&uid, &counter);
+  return 0;
+}
+"""
+
+b = BPF(text=program)
+b.attach_raw_tracepoint(tp="sys_enter", fn_name="hello")
+
+while True:
+    time.sleep(2)
+    for k, v in b["counter_table"].items():
+        print("ID: %d, %d\t" % (k.value, v.value), end='')
+    print("")
