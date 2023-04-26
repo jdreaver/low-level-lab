fd8f53782b704f4b9d165d157fdd76eb4be8caee
David Reaver <me@davidreaver.com>
2023-04-25 17:02:00 -0700

    Print Hello in boot

--- bootloaders/64-bit-os-barebones/README.md
+++ bootloaders/64-bit-os-barebones/README.md
@@ -1,4 +1,6 @@
 # 64 bit OS bare bones
 
 Loosely following these resources:
+- <https://github.com/cfenollosa/os-tutorial>
+  - This is inspired by this document: [Writing a Simple Operating System — from Scratch](https://www.cs.bham.ac.uk/~exr/lectures/opsys/10_11/lectures/os-dev.pdf)
 - <https://github.com/gmarino2048/64bit-os-tutorial>
--- bootloaders/64-bit-os-barebones/boot.asm
+++ bootloaders/64-bit-os-barebones/boot.asm
@@ -1,10 +1,51 @@
+; The BIOS loads the first 512 bytes of the disk into memory at address 0x7C00.
+; We have to tell NASM about this so it knows to offset any addresses by 0x7C00.
+[org 0x7C00]
+
+; All x86-type chips start in 16-bit "real mode", which
+; enables backwards compatibility all the way to the Intel 8086 chip.
+; Yes, even the 64-bit variants have this ability, it's just not
+; commonly used. So, we'll need to tell NASM to generate 16 bit instructions.
+[BITS 16]
+
+; Print 'Hello'
+;
+; When in BIOS (https://wiki.osdev.org/BIOS), we can BIOS functions to perform
+; certain actions. These functions are activated with interrupts. You set the
+; values of certain registers to select which function to use and to pass args.
+;
+; The BIOS function to print a character is 0x10. The subfunction to print a
+; character is 0x0e. The character to print is passed in the al register.
+
+mov ah, 0x0e                    ; tty mode
+mov al, 'H'                     ; character to print
+int 0x10                        ; call BIOS interrupt
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10                       ; al is still 'l' from the previous call
+mov al, 'o'
+int 0x10
+mov al, '!'
+int 0x10
+
 ; A simple boot sector program that loops forever
 loop:
     jmp loop
 
+; The magic number must come at the end of the boot
+; sector. We need to fill the space with zeros to ensure that
+; the magic number comes at the end. Remember, since the magic
+; number is 2 bytes, the rest of the boot sector is 510.
 
-; Fill with 510 zeros minus the size of the previous code
+; We use the 'times' command to repeat the 'db' or "define byte" command
+; to pad the rest. Remeber, we will use the '$' and '$$' commands to
+; determine how much space is left between the final instruction and
+; the magic number.
 times 510-($-$$) db 0
 
-; Magic number
+; The 'dw' command stands for "define word", and puts
+; a 16-bit literal integer at that specific location in the file.
+; We use 'dw' here to define the magic number, 'AA55'
 dw 0xaa55
