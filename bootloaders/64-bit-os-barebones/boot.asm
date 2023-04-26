8f6cfff2ac0ac3818ecee45407485a110dc31654
David Reaver <me@davidreaver.com>
2023-04-25 17:10:39 -0700

    Function to print in BIOS

--- bootloaders/64-bit-os-barebones/boot.asm
+++ bootloaders/64-bit-os-barebones/boot.asm
@@ -17,22 +17,58 @@
 ; The BIOS function to print a character is 0x10. The subfunction to print a
 ; character is 0x0e. The character to print is passed in the al register.
 
-mov ah, 0x0e                    ; tty mode
-mov al, 'H'                     ; character to print
-int 0x10                        ; call BIOS interrupt
-mov al, 'e'
-int 0x10
-mov al, 'l'
-int 0x10
-int 0x10                       ; al is still 'l' from the previous call
-mov al, 'o'
-int 0x10
-mov al, '!'
-int 0x10
+mov bx, hello
+call print_bios
+
+mov bx, goodbye
+call print_bios
+
+jmp loop
+
+; Define function print_bios
+; Input pointer to string in bx
+print_bios:
+        ; We'll be using registers ax and bx, so we'll need to save their
+        ; current values on the stack so we don't mess anything up once the
+        ; function is complete. We use the 'push' and 'pop' instructions for
+        ; this.
+        push ax
+        push bx
+
+        ; The BIOS function to print a character is 0x10. The subfunction to
+        ; print a character is 0x0e. The character to print is passed in the al
+        ; register.
+        mov ah, 0x0e
+
+print_bios_loop:
+        mov al, [bx]            ; Load the next char to print in al
+        cmp al, 0               ; Check if the char is null
+        je print_bios_done      ; If it is, we're done
+        int 0x10                ; Otherwise, print the char
+        inc bx                  ; Increment the pointer to the next char
+        jmp print_bios_loop     ; Loop back to the beginning
+
+print_bios_done:
+        ; Restore the values of ax and bx registers.
+        ; order.
+        pop bx
+        pop ax
+
+        ; This instruction returns to the instruction after the initial 'call'
+        ; instruction that got us here. It works almost the same as 'return' in
+        ; C, but you can't give it a value.
+        ret
 
 ; A simple boot sector program that loops forever
 loop:
     jmp loop
+
+; Data
+hello:
+    db `Hello, World!\r\n`, 0
+
+goodbye:
+    db `Goodbye!\r\n`, 0
 
 ; The magic number must come at the end of the boot
 ; sector. We need to fill the space with zeros to ensure that
